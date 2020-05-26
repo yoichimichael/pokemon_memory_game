22cda5da2adf5093efdc2e7c2cc4fc1074373258
Garen Sahagian <g.s.sahagian@gmail.com>
2020-05-26 16:43:29 -0400

    Continuing work on sign in form

--- front_end/index.js
+++ front_end/index.js
@@ -6,18 +6,41 @@ document.addEventListener("DOMContentLoaded", ()=>{
 })
 
 // New user creation
-document.addEventListener('submit', (e)=>{
-    e.preventDefault()
-    const newUserName = e.target[0].value
-    fetch(USERS_URL,{
-        method: "POST",
-        headers: headers,
-        body: JSON.stringify({
-            username: e.target[0].value
+
+document.addEventListener('click', (e)=>{
+    if (e.target.textContent ==="Sign Up" ){
+        e.preventDefault()
+        const inputName = document.querySelector("#login-field").value
+        fetch(USERS_URL)
+        .then(response => response.json())
+        .then(users => {
+            const foundUser = users.find(function (user){return user.username === inputName})
+            if (foundUser){
+                // Change display to "logged in state"
+            } else{
+                // Display error message on DOM
+            }
         })
-    })
-    .then(response => response.json())
-    .then(console.log)
-    // change display to reflect created user
-    // hide sign-up and sign-in form
+    } else if (e.target.textContent === "Log In"){
+
+    }
 })
+// document.addEventListener('submit', (e)=>{
+//     e.preventDefault()
+//     const newUserName = e.target[0].value
+//     fetch(USERS_URL,{
+//         method: "POST",
+//         headers: headers,
+//         body: JSON.stringify({
+//             username: e.target[0].value
+//         })
+//     })
+//     .then(response => response.json())
+//     .then(newUser =>{
+//         document.querySelector('#user-div').innerHTML=`<p>Welome ${newUser.username}</p>`
+//         document.querySelector('#user-div').dataset.userId = newUser.id
+//         document.querySelector('#sign-up').style.display = "none"
+//     })
+//     // change display to reflect created user
+//     // hide sign-up and sign-in form
+// })
