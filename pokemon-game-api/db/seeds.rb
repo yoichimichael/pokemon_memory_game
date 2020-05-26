8c8163868cae271323290d69de56fe6eec8ab654
Garen Sahagian <g.s.sahagian@gmail.com>
2020-05-26 14:41:27 -0400

    Initializing User Model in backend

--- pokemon-game-api/db/seeds.rb
+++ pokemon-game-api/db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+User.destroy_all
+
+5.times do 
+    User.create(username:Faker::Name.first_name) 
+end
