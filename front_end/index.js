9f7ce0270e384f4ab8bd37b32956af546a46389d
Garen Sahagian <g.s.sahagian@gmail.com>
2020-05-26 17:44:13 -0400

    User authorization now functional

--- front_end/index.js
+++ front_end/index.js
@@ -8,21 +8,54 @@ document.addEventListener("DOMContentLoaded", ()=>{
 // New user creation
 
 document.addEventListener('click', (e)=>{
+    const inputName = document.querySelector("#login-field").value
+    const userDiv = document.querySelector('#user-div')
+    const authDiv = document.querySelector('#auth-div')
     if (e.target.textContent ==="Sign Up" ){
-        e.preventDefault()
-        const inputName = document.querySelector("#login-field").value
+        // e.preventDefault()
         fetch(USERS_URL)
         .then(response => response.json())
         .then(users => {
             const foundUser = users.find(function (user){return user.username === inputName})
             if (foundUser){
-                // Change display to "logged in state"
+                // document.querySelector('#error').style.display = "block"
+                alert("This username is already taken!")
+                document.querySelector("#login-field").value = ""
             } else{
-                // Display error message on DOM
+                fetch(USERS_URL, {
+                    method: "POST",
+                    headers: headers,
+                    body: JSON.stringify({
+                        username: inputName
+                    })
+                })
+                .then(response => response.json())
+                .then(newUser => {
+                    userDiv.innerHTML=`<p>Welome ${newUser.username}!</p><br><button id="log-out">Log Out</button>`
+                    userDiv.dataset.userId = newUser.id
+                    authDiv.style.display = "none"
+                })
             }
         })
     } else if (e.target.textContent === "Log In"){
-
+        fetch(USERS_URL)
+        .then(response => response.json())
+        .then(users => {
+            const foundUser = users.find(function (user){return user.username === inputName})
+            if (foundUser){
+                // document.querySelector('#error').style.display = "block"
+                userDiv.innerHTML=`<p>Welome ${foundUser.username}!</p><br><button id="log-out">Log Out</button>`
+                userDiv.dataset.userId = foundUser.id
+                authDiv.style.display = "none"
+                document.querySelector("#login-field").value = ""
+            } else{
+                alert("There is no user with that username. Check your spelling! LOSER!")
+            }
+        })
+    } else if (e.target.textContent === "Log Out"){
+        authDiv.style.display = "block"
+        userDiv.innerHTML = ""
+        userDiv.dataset.userId = ''
     }
 })
 // document.addEventListener('submit', (e)=>{
