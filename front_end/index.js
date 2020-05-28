e1e4427070c3144e2b7daf5fdd6aa12f333940e2
yoichimichael <yoichi.michael.nagano@gmail.com>
2020-05-28 14:45:17 -0700

    back on track baby

--- front_end/index.js
+++ front_end/index.js
@@ -63,71 +63,9 @@ document.addEventListener('click', (e)=>{
     } else if (e.target.id === "start-button"){
         e.target.style.display = 'none'
         startGameLoop()
-        gameMaster()
+        // gameMaster()
     }
 })
-
-// `°~,_,~°`This is the actual game logic`°~,_,~°`
-let flippedCards = []
-let score = 0
-let strikes = 0 
-let moves = 0
-const startGameLoop = () =>{
-    const cardsContainer = document.getElementById('cards-container') 
-    const cardImageSrcs = addPokemonCards()
-    window.setTimeout(function(){hideCards();}, 2000);
-    console.log(cardImageSrcs)
-}
-
-const hideCards = () =>{
-    const cards = document.querySelectorAll('.card')
-    for(let i=0;i<cards.length;i++){
-        cards[i].classList.toggle('is-flipped')
-        cards[i].addEventListener('click', (e) => {
-            flipCard(cards[i])
-    })
-}}
-
-const flipCard = (node) =>{
-    node.classList.toggle('is-flipped')
-    node.classList.toggle('disabled')
-    flippedCards.push(node)
-    if (flippedCards.length === 2){
-        moves++
-        if (flippedCards[0].children[0].style.backgroundImage === flippedCards[1].children[0].style.backgroundImage){
-            console.log(flippedCards)
-            score++
-            flippedCards = []
-            // change styling to demonstrate match
-        }else{
-            setTimeout(function(){
-                flippedCards[0].classList.toggle('disabled')
-                flippedCards[0].classList.toggle('is-flipped')
-                flippedCards[1].classList.toggle('disabled')
-                flippedCards[1].classList.toggle('is-flipped')
-                strikes++
-                flippedCards = []
-            }, 1200)
-        }
-    }
-}
-
-const gameMaster = () => {
-    
-    // Show Timer on page
-    // setInterval(timer,1000)
-
-    // Show End Game Screen, High Scores, Start New Game
-    // setInterval(gameOver, 300000)
-}
-
-const timer = ()=>{
-
-}
-
-const gameOver = () =>{
-    
-}
 
 // This stuff loads cards to the board
 
